5e7d9c4c3e26191a63e90bbeaeba27f307d2ad94
Karthikeyan <76770213+karthikeyan12068@users.noreply.github.com>
2022-08-15 08:07:37 +0530

    Create frog-jump.cpp
    Problem : https://www.youtube.com/watch?v=EgG3jsGoPvQ&list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY

---
+++ DP/frog-jump.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+typedef vector<int> v_i;
+typedef vector<long long int> v_ll;
+typedef vector<pair<int,int>> vp_i;
+typedef vector<pair<long long int,long long int>> vp_ll;
+typedef map<int,int> m_i;
+typedef map<long long int,long long int> m_ll;
+typedef map<pair<int,int>,int> mp_i;
+typedef map<pair<long long int,long long int>,long long int> mp_ll;
+typedef set<int> s_i;
+typedef set<long long int> s_ll;
+typedef set<pair<int,int>> sp_i;
+typedef set<pair<long long int,long long int>> sp_ll;
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 2e18
+#define sorts(v) sort((v).begin(),(v).end())
+#define rev(v) reverse((v).begin(),(v).end())
+#define count2(v,s) count((v).begin(),(v).end(),(s))
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(min(a,b),min(c,d))
+#define max4(a,b,c,d)   max(max(a,b),max(c,d))
+#define max5(a,b,c,d,e) max(max3(a,b,c),max(d,e))
+#define min5(a,b,c,d,e) min(min3(a,b,c),min(d,e))
+#define forn(a,b,c) for(ll (c)=(a);(c)<(b);(c)++)
+#define forr(a,b,c) for(ll (c)=(a);(i)>=(b);(c)--)
+#define rep1(s) for(auto &it:(s))
+#define rep2(a,b,c) for(ll i=(a);i<(b);i+=(c))
+ll binets_formula(ll n) {
+    double sqrt5 = sqrt(5);
+    ll F_n = (pow((1 + sqrt5), n) - pow((1 - sqrt5), n)) / (pow(2, n) * sqrt5);
+    return F_n;
+}
+string str(char c){
+    string s;
+    stringstream ss;
+    ss << c;
+    ss >> s;
+    return s;
+}
+bool sortbysec(const pair<int,int> &a,const pair<int,int> &b)
+{
+    return (a.second < b.second);
+}
+string to_binary(ll val)
+{
+    return (std::bitset<30>(val).to_string());
+}
+ll to_decimal(const string& s)
+{
+    return (std::bitset<30>(s).to_ulong());
+}
+double log_a_to_base_b(double a, double b)
+{
+    return log(a) / log(b);
+}
+ll str_to_in(string s)
+{
+    stringstream geek(s);
+    ll x = 0ll;
+    geek >> x;
+    return x;
+}
+
+ll sum_v(v_ll v1){
+    ll sum=0;
+    forn(0,v1.size(),i){
+        sum+=v1[i];
+    }
+    return sum;
+}
+
+ll jump(ll n,v_ll v1,v_ll dp){
+    if(n==0){
+        return 0;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    return dp[n]=min(jump(n-1,v1,dp)+abs(v1[n]-v1[n-1]), (n-2>=0)?(jump(n-2,v1,dp)+abs(v1[n]-v1[n-2])):INT_MAX);
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll n;
+    cin>>n;
+    v_ll v1(n);
+    forn(0,n,i){
+        cin>>v1[i];
+    }
+   v_ll dp(n);
+    forn(0,n,i){
+        dp[i]=-1;
+    }
+    n--;
+    cout<<jump(n,v1,dp);
+}
